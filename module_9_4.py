044f2acdb0b0e42bbe163120cb97009947abf115
SafonovValera <o33o.post@gmail.com>
2024-12-26 15:03:37 +0300

    Update module_9_4.py

--- module_9_4.py
+++ module_9_4.py
@@ -36,7 +36,7 @@ def get_advanced_writer(file_name):
 
         with open(file_name, 'a', encoding='utf8') as file:
             for i in data_set:
-                file.write(str(i)+'\n')
+                file.write(str(i) + '\n')
 
 # Функция get_advanced_writer возвращает функцию write_everything.
 
